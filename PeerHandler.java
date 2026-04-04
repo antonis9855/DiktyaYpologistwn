5c2e38c7cd716f762152972f1ec09d3e1c58ee09
Petroutolis <Petroutolis@hotmail.com>
2026-04-04 12:30:10 +0300

    Implement user registration, login, and logout
    added register, login, logout and msg split

--- PeerHandler.java
+++ PeerHandler.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.Random;
 
 public class PeerHandler implements Runnable {
 
@@ -19,13 +20,89 @@ public class PeerHandler implements Runnable {
             String line;
             while ((line = in.readLine()) != null) {
                 System.out.println("[SERVER] Received: " + line);
-                out.println("OK:" + line);
+                
+                String[] pieces = line.split("\\|");
+                String command = pieces[0];
+                
+                switch (command){
+                    case "REGISTER":
+                        register (pieces);
+                        break;
+                    case "LOGIN":
+                        login (pieces);
+                        break;
+                    case "LOGOUT":
+                        logout (pieces);
+                        break;
+                    default:
+                        out.println("Error command");
+                }           
             }
-
         } catch (IOException e) {
             System.out.println("[SERVER] Peer disconnected: " + e.getMessage());
         } finally {
             try { socket.close(); } catch (IOException e) {}
+        }
+    }
+
+   private void register(String[] pieces) {
+        if (pieces.length != 3) {
+            out.println("ERROR|Format is REGISTER|username|password");
+            return;
+        }
+        
+        String username = pieces[1];
+        String password = pieces[2];
+
+       if (!AuctionServer.accounts.containsKey(username)) {
+            AuctionServer.accounts.put(username, new User(username, password));
+            out.println("SUCCESS|Account created for " + username);
+        } else {
+            // Επιστρέφει μήνυμα αποτυχίας αν το username χρησιμοποιείται [cite: 44]
+            out.println("ERROR|Username already exists. Please choose a different username.");
+        }
+    }
+
+   private void login(String[] pieces) {
+        if (pieces.length != 4) {
+            out.println("ERROR|Format is LOGIN|username|password|port");
+            return;
+        }
+        String username = pieces[1];
+        String password = pieces[2];
+        int peerPort;
+        try {
+            peerPort = Integer.parseInt(pieces[3]);
+        } catch (NumberFormatException e) {
+            out.println("ERROR|Invalid port number.");
+            return;
+        }
+        User user = AuctionServer.accounts.get(username);
+      
+        if (user != null && user.password.equals(password)) {
+            Random random = new Random();
+            int randomNum = random.nextInt(1000000000);
+            String tokenId = String.valueOf(randomNum);
+            String ipAddress = socket.getInetAddress().getHostAddress();
+            AuctionServer.activeSessions.put(tokenId, new Session(tokenId, username, ipAddress, peerPort));
+            out.println("SUCCESS|" + tokenId);
+        } else {
+            out.println("ERROR|Invalid username or password.");
+        }
+    }
+
+   private void logout(String[] pieces) {
+       if (pieces.length != 2) {
+           out.println("ERROR|Format is LOGOUT|tokenId");
+           return;
+        }
+        String tokenId = pieces[1];
+
+       if (AuctionServer.activeSessions.containsKey(tokenId)) {
+           AuctionServer.activeSessions.remove(tokenId);
+           out.println("SUCCESS|Logged out successfully.");
+        } else {
+           out.println("ERROR|Invalid Token ID.");
         }
     }
 }
