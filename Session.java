a50cda1b0f66761477dd9592d552f611a42b6733
Petroutolis <Petroutolis@hotmail.com>
2026-04-04 10:08:22 +0300

    Add Session class with tokenId, username, and ipAddress
    We store sessions informations, token_id, ip_address, port(opened by peer), username,

---
+++ Session.java
@@ -0,0 +1,14 @@
+
+public class Session {
+  public String tokenId;
+  public String username;
+  public String ipAddress;
+  public int port;
+
+  public Session (String tokenId, String username, String ipAddress, int port) {
+    this.tokenId = tokenId;
+    this.username = username;
+    this.ipAddress = ipAddress;
+    this.port = port;
+  }
+} 
