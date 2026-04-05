2cd8969aab856abdc1ec3eee5cf64284afc93f5e
Petroutolis <Petroutolis@hotmail.com>
2026-04-05 17:06:01 +0300

    Create AuctionItem class with auction properties

---
+++ AuctionItem.java
@@ -0,0 +1,15 @@
+public class AuctionItem {
+    public String tokenId; 
+    public String objectId;
+    public String description;
+    public int startBid;
+    public int auctionDuration;
+
+    public AuctionItem(String tokenId, String objectId, String description, int startBid, int auctionDuration) {
+        this.tokenId = tokenId;
+        this.objectId = objectId;
+        this.description = description;
+        this.startBid = startBid;
+        this.auctionDuration = auctionDuration;
+    }
+}
