d15b33df9f8a4bd0bf1e73f3012a27f8185e6ed6
Petroutolis <Petroutolis@hotmail.com>
2026-04-05 23:30:56 +0300

    Refactor bidding logic and add file download method

--- Bidder.java
+++ Bidder.java
@@ -15,6 +15,7 @@ public class Bidder implements Runnable {
         this.serverIp = serverIp;
         this.serverPort = serverPort;
         this.random = new Random();
+
     }
 
     @Override
@@ -34,30 +35,65 @@ public class Bidder implements Runnable {
                         String[] parts = response.split("\\|");
                         String currentObjectId = parts[1];
                         int highestBid = Integer.parseInt(parts[2]);
+                        out.println("CHECK_WINNER|" + tokenId);
+                        String winnerResponse = in.readLine();
+
+                        if (winnerResponse != null && winnerResponse.startsWith("SUCCESS")) {
+                            String[] winParts = winnerResponse.split("\\|");
+                            String wonObjectId = winParts[1];
+                            String sellerIp = winParts[2];
+                            int sellerPort = Integer.parseInt(winParts[3]);
+
+                            System.out.println("\n [" + username + "-Buyer] Congrats! You won :  " + wonObjectId + "!");
+                            downloadFileFromSeller(sellerIp, sellerPort, wonObjectId);
+                            out.println("TRANSACTION_COMPLETE|" + tokenId + "|" + wonObjectId);
+                        }
+                        // ------------------------------------------
 
                         if (random.nextDouble() <= 0.60) {
-
                             double randValue = random.nextDouble();
                             int newBid = (int) (highestBid * (1.0 + (randValue / 10.0)));
-
                             if (newBid <= highestBid) {
                                 newBid = highestBid + 1;
                             }
-
-                            System.out.println("\n Buyer : " + username + "wants this : " + currentObjectId + "and offers:  " + newBid);
-
+                            System.out.println("\n[" + username + "-Buyer] I want : " + currentObjectId + "and i bid: " + newBid);
                             out.println("PLACE_BID|" + tokenId + "|" + newBid);
                             String bidResponse = in.readLine();
-                            System.out.println("Server responded : " + bidResponse);
+                            System.out.println("[" + username + "-Buyer] Server responded " + bidResponse);
                         } else {
-                            System.out.println("\n" + username + "Saw the object " + currentObjectId + " but not interested.");
+                            System.out.println("\n[" + username + "-Buyer] I saw : " + currentObjectId + " But not interested.");
                         }
                     }
+
                 } catch (IOException e) {
                 }
             }
+
         } catch (InterruptedException e) {
             System.out.println(">> Bidder thread stopped.");
+        }
+    }
+    private void downloadFileFromSeller(String ip, int port, String objectId) {
+        File buyerDir = new File("shared_directory_" + username);
+        if (!buyerDir.exists()) buyerDir.mkdir();
+
+        File newFile = new File(buyerDir, objectId + ".txt");
+
+        try (Socket p2pSocket = new Socket(ip, port);
+             PrintWriter p2pOut = new PrintWriter(p2pSocket.getOutputStream(), true);
+             BufferedReader p2pIn = new BufferedReader(new InputStreamReader(p2pSocket.getInputStream()));
+             FileWriter fileWriter = new FileWriter(newFile)) {
+
+            p2pOut.println("DOWNLOAD|" + objectId);
+            System.out.println("[" + username + "-Buyer] Downloading file " + objectId + ".txt...");
+            String line;
+            while ((line = p2pIn.readLine()) != null) {
+                if (line.equals("EOF")) break; // Το EOF (End Of File) σημαίνει ότι τελείωσε
+                fileWriter.write(line + "\n");
+            }
+            System.out.println("[" + username + "-Buyer] Saving file completed!");
+        } catch (IOException e) {
+            System.out.println("[" + username + "-Buyer] Failed to connect P2P: " + e.getMessage());
         }
     }
 }
