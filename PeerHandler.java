14b198fd5a5e9d28d3b49b6833d2707eff69d507
Petroutolis <Petroutolis@hotmail.com>
2026-04-05 23:43:01 +0300

    Update PeerHandler.java
    Removed commented-out code for clarity and improved readability.

--- PeerHandler.java
+++ PeerHandler.java
@@ -223,7 +223,6 @@ private void getCurrentAuction(String[] pieces) {
 
         ActiveAuction wonAuction = null;
 
-        // Ψάχνουμε στη λίστα αν υπάρχει δημοπρασία που κέρδισε αυτό το tokenId
         synchronized(AuctionServer.completedAuctions) {
             for (ActiveAuction a : AuctionServer.completedAuctions) {
                 if (tokenId.equals(a.highestBidderTokenId)) {
@@ -231,7 +230,6 @@ private void getCurrentAuction(String[] pieces) {
                     break;
                 }
             }
-            // Αν βρήκαμε ότι κέρδισε, το βγάζουμε από τη λίστα για να μην τον ξαναειδοποιήσουμε
             if (wonAuction != null) {
                 AuctionServer.completedAuctions.remove(wonAuction);
             }
@@ -242,8 +240,7 @@ private void getCurrentAuction(String[] pieces) {
             Session sellerSession = AuctionServer.activeSessions.get(sellerToken);
 
             if (sellerSession != null) {
-                // Του στέλνουμε το SUCCESS μαζί με το Όνομα Αρχείου, την IP και το Port του Πωλητή
-                out.println("SUCCESS|" + wonAuction.item.objectId + "|" + sellerSession.ipAddress + "|" + sellerSession.port);
+               out.println("SUCCESS|" + wonAuction.item.objectId + "|" + sellerSession.ipAddress + "|" + sellerSession.port);
             } else {
                 out.println("ERROR|Saler is offline.");
             }
