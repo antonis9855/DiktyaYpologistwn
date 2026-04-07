381d2dcb2513c4856b62d015c8666f69081fba2a
antonis <bolanosantonis@gmail.com>
2026-04-08 02:29:48 +0300

    Session.java - Apothikevei to active session enos peer: token, username, ip kai port tou peer listener tou.

--- Session.java
+++ Session.java
@@ -1,14 +1,13 @@
-
 public class Session {
-  public String tokenId;
-  public String username;
-  public String ipAddress;
-  public int port;
+    public String tokenId;
+    public String username;
+    public String ipAddress;
+    public int port;
 
-  public Session (String tokenId, String username, String ipAddress, int port) {
-    this.tokenId = tokenId;
-    this.username = username;
-    this.ipAddress = ipAddress;
-    this.port = port;
-  }
-} 
+    public Session(String tokenId, String username, String ipAddress, int port) {
+        this.tokenId = tokenId;
+        this.username = username;
+        this.ipAddress = ipAddress;
+        this.port = port;
+    }
+}
