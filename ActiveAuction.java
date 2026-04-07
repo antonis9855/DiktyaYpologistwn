a10898f85c50be27d139fcee080712416e06ef49
antonis <bolanosantonis@gmail.com>
2026-04-08 02:29:48 +0300

    ActiveAuction.java - Apothikevei tin katastasi mias trexousas dimoprasias: antikeimeno, trexon highest bid, token tou highest bidder kai chronos lixis.

--- ActiveAuction.java
+++ ActiveAuction.java
@@ -3,13 +3,19 @@ public class ActiveAuction {
     public int currentBid;
     public String highestBidderUsername;
     public String highestBidderTokenId;
-    public boolean isFinished;
+    public volatile boolean isFinished;
+    public long endTime;
 
     public ActiveAuction(AuctionItem item) {
         this.item = item;
         this.currentBid = item.startBid;
-        this.highestBidderUsername = null; 
+        this.highestBidderUsername = null;
         this.highestBidderTokenId = null;
         this.isFinished = false;
+        this.endTime = System.currentTimeMillis() + (item.auctionDuration * 1000L);
+    }
+
+    public long getTimeRemainingSeconds() {
+        return Math.max(0, (endTime - System.currentTimeMillis()) / 1000);
     }
 }
